5d0e1044c854f9c2b2dcc5539f262c43338b1d93
Mikeandike <Mikeplusikes@gmail.com>
2019-09-29 23:36:56 -0500

    Changed morning to only declare the reddit api bot -- and call the report package for all other api interaction. Ditched the idea of using a cmd flag parser since I don't think this part of the module will need it, but rather use rest calls

--- cmd/morning/main.go
+++ cmd/morning/main.go
@@ -3,24 +3,57 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/thecsw/mira"
-	"gopkg.in/alecthomas/kingpin.v2"
-)
+	"github.com/MrMikeandike/wsbMorningArchive/pkg/report"
 
-var (
-	confFlag = kingpin.Flag("conf", "path to conf file").Short(rune('c')).Default(".conf").ExistingFile()
+	"github.com/turnage/graw/reddit"
 )
 
 func main() {
-	kingpin.Parse()
-	cred := mira.ReadCredsFromFile(*confFlag)
-	fmt.Println(cred.ClientId)
+	id, _ := os.LookupEnv("CLIENT_ID")
+	secret, _ := os.LookupEnv("CLIENT_SECRET")
+	username, _ := os.LookupEnv("USER_NAME")
+	password, _ := os.LookupEnv("PASSWORD")
+	agent, _ := os.LookupEnv("USER_AGENT")
 
-	reddit, err := mira.Authenticate(&cred)
+	cfg := reddit.BotConfig{
+		Agent: agent,
+		App: reddit.App{
+			ID:       id,
+			Secret:   secret,
+			Username: username,
+			Password: password,
+		},
+	}
+	fmt.Println(cfg)
+	bot, err := reddit.NewBot(cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
-	_ = reddit
+	//page, err := GetPageBefore(bot, "t3_cmzm4z", "23")
+	page, err := report.GetSpecific(bot, []string{
+		"t3_d0gt23",
+		"t3_d01c9u",
+		"t3_czl07a",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if page.PostCount == 0 {
+		log.Fatal("post count == 0")
+	}
+	// fmt.Printf("First: %s\n", page.FirstID)
+	// fmt.Printf("Last: %s\n", page.LastID)
+	// fmt.Printf("Count: %d\n", page.PostCount)
+	//for i, v := range page.Reports {
+	for i, v := range page.Reports {
+		fmt.Println("----------------------")
+		fmt.Printf("INDEX: %d\n", i)
+		fmt.Printf("Title: %s\n", v.Title)
+		fmt.Printf("ID   : %s\n", v.FullID)
+		fmt.Printf("Time : %s\n", v.DateTime.String())
+		fmt.Println("----------------------")
+	}
 
 }
